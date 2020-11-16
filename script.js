788c3f44ca62f11469081512b2dba88b924adb8f
Minsi Yang <yangminsi@gmail.com>
2020-11-16 22:18:55 +0000

    Added #generateRandomQuotes

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const messages = [["Your time is limited, so don’t waste it living someone else’s life.", "Steve Jobs", "motivated"], 
-                  ["The mind is everything. What you think you become.", "Buddha", "zen"], 
-                  ["Eighty percent of success is showing up.", "Woody Allen", "determined"]]
+const messages = [["are motivated today!", "are modelling on Steve Jobs.", 'believe "Your time is limited, so don’t waste it living someone else’s life."'], 
+                  ["are zen today.", , "are modelling on Buddha.", 'believe "The mind is everything. What you think you become."' ], 
+                  ["are determined today!", , "are modelling on Woody Allen.", 'believe "Eighty percent of success is showing up."' ]]
+
 
 const generateRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
 
+const generateRandomQuotes = (arr) => {
+  let index = generateRandomIndex(arr)
+  arr[index].forEach(string => {
+    console.log(`You ${string}`)
+  })
+}
+
+generateRandomQuotes(messages)
