d4c58f97810da8b5a5a683994a475aa5dc2c496b
Minsi Yang <yangminsi@gmail.com>
2020-11-16 21:49:56 +0000

    Implement #generateRandomIndex

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-var a = `Node REPL is awesome!`
+const messages = [["Your time is limited, so don’t waste it living someone else’s life.", "Steve Jobs", "motivated"], 
+                  ["The mind is everything. What you think you become.", "Buddha", "zen"], 
+                  ["Eighty percent of success is showing up.", "Woody Allen", "determined"]]
+
+const generateRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
 
