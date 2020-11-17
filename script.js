1db00d930337a31fa20ce23ee6e15bbdb0181770
Minsi Yang <yangminsi@gmail.com>
2020-11-17 10:07:20 +0000

    Correcting typos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const messages = [["are motivated today!", "are modelling on Steve Jobs.", 'believe "Your time is limited, so don’t waste it living someone else’s life."'], 
-                  ["are zen today.", , "are modelling on Buddha.", 'believe "The mind is everything. What you think you become."' ], 
-                  ["are determined today!", , "are modelling on Woody Allen.", 'believe "Eighty percent of success is showing up."' ]]
+                  ["are zen today.", "are modelling on Buddha.", 'believe "The mind is everything. What you think you become."' ], 
+                  ["are determined today!", "are modelling on Woody Allen.", 'believe "Eighty percent of success is showing up."' ]]
 
 
 const generateRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
